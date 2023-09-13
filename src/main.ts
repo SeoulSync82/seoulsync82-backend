78d0fb194ae984f7673c1ba970b5525a947085fb
blanc2 <blanc2@newming.co.kr>
2023-09-13 18:21:13 +0900

    feature > 0.0.1 > Fastify설치. Swagger 연결, Congifg module 설치,

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: false }),
+    {
+      forceCloseConnections: false,
+      logger:
+        process.env.NODE_ENV === 'production'
+          ? ['error', 'warn', 'log', 'debug']
+          : ['error', 'warn', 'log', 'verbose', 'debug'],
+    },
+  );
+  const config = new DocumentBuilder()
+    .setTitle('newming-api')
+    .setDescription('The Next Newming API description')
+    .setVersion('1.0.0')
+    .addTag('swagger')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3001);
 }
 bootstrap();
