6a79f665d68ccceaa631cc60ca2775a572dfbfef
blanc2 <blanc2@newming.co.kr>
2023-09-27 20:34:02 +0900

    feature > 0.0.1 > reaction 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PlaceModule } from './place/place.module';
 import { CourseModule } from './course/course.module';
+import { ReactionModule } from './reaction/reaction.module';
 
 @Module({
-  imports: [UserModule, PlaceModule, CourseModule],
+  imports: [UserModule, PlaceModule, CourseModule, ReactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
