220f1f83ce00fc7421c0513ccc74614fe57488f6
blanc2 <blanc2@newming.co.kr>
2023-09-28 01:25:23 +0900

    feature > 0.0.1 > notification 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { PlaceModule } from './place/place.module';
 import { CourseModule } from './course/course.module';
 import { ReactionModule } from './reaction/reaction.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [UserModule, PlaceModule, CourseModule, ReactionModule],
+  imports: [UserModule, PlaceModule, CourseModule, ReactionModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
