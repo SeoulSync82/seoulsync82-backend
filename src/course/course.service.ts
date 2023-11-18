8fa4d901b9a13955a513d2653d3cc3bb04cc1820
Blanc <topblade6@naver.com>
2023-11-19 04:06:25 +0900

    feature > 0.0.1 > AI 코스추천 - 장소 가중치 측정 및 상위 N개 랜덤 추천 로직구현

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DetailResponseDto } from 'src/commons/dto/response.dto';
+import { PlaceEntity } from 'src/entities/place.entity';
 import { PlaceQueryRepository } from 'src/place/place.query.repository';
 import { SubwayQueryRepository } from 'src/place/subway.query.repository';
 import { CourseQueryRepository } from './course.query.repository';
@@ -27,8 +28,43 @@ export class CourseService {
 
     if (dto.customs.includes('문화')) {
     }
-    const subwayPlaceList = await this.placeQueryRepository.findPlacesOnSubwayLine(customs, dto);
-    console.log(subwayPlaceList);
+    const subwayPlaceList: PlaceEntity[] = await this.placeQueryRepository.findPlacesOnSubwayLine(
+      customs,
+      dto,
+    );
+
+    function calculateWeight(subwayPlaceList) {
+      return subwayPlaceList.score * Math.log(subwayPlaceList.review_count + 1);
+    }
+
+    function getTopWeight(subwayPlaceList, topN) {
+      const weightedPlace = subwayPlaceList.map((item) => ({
+        ...item,
+        weight: calculateWeight(item),
+      }));
+
+      return weightedPlace.sort((a, b) => b.weight - a.weight).slice(0, topN);
+    }
+
+    const topWeightPlaces = getTopWeight(subwayPlaceList, 10);
+    // 가중치 평균을 측정해 상위 N개 추출
+
+    function getRandomElements(topWeightPlaces, n) {
+      let tempArray = [...topWeightPlaces];
+      const result = [];
+
+      for (let i = 0; i < n && tempArray.length > 0; i++) {
+        const randomIndex = Math.floor(Math.random() * tempArray.length);
+        result.push(tempArray[randomIndex]);
+        tempArray.splice(randomIndex, 1);
+        // 선택된 요소 제거 - 코스에 같은 type 여러개 있는 case 고려
+      }
+
+      return result;
+    }
+
+    const randomElements = getRandomElements(topWeightPlaces, 1);
+    // 상위 N개중 랜덤한 값 추출
 
     // 평점 리뷰 없는 "문화"
 
@@ -39,6 +75,6 @@ export class CourseService {
 
     // 나중에 가중치 삭감 반영
 
-    return DetailResponseDto.from(subwayPlaceList);
+    return DetailResponseDto.from(randomElements);
   }
 }
