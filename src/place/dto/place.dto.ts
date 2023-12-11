13334acae7cc71756820d90360fcbca4dcfc2d7a
Blanc <topblade6@naver.com>
2023-12-12 03:05:22 +0900

    feature > 0.0.1 > 전시소개 목록, 팝업 소개 목록 - @res 추가

--- src/place/dto/place.dto.ts
+++ src/place/dto/place.dto.ts
@@ -267,10 +267,24 @@ export class ExhibitionDto {
 
   @Expose()
   @ApiProperty({
+    example: '팝업',
+    description: '장소 종류',
+  })
+  place_type: string;
+
+  @Expose()
+  @ApiProperty({
     example: '쫄깃즈 키링 팝업스토어',
     description: '장소 이름',
   })
   place_name: string;
+
+  @Expose()
+  @ApiProperty({
+    example: '서울특별시 종로구 돈화문로11나길 28-1 1호 익선스페이스 A홀',
+    description: '주소',
+  })
+  address: string;
 
   @Expose()
   @ApiProperty({
@@ -293,6 +307,37 @@ export class ExhibitionDto {
     description: '마감일',
   })
   end_date: Date;
+
+  @Expose()
+  @ApiProperty({
+    example: ' 월~금요일 10:00~18:00· 토요일 12:00~19:00',
+    description: '전시 영업 시간',
+  })
+  @Transform(({ value }) => value ?? undefined)
+  operation_time?: string;
+
+  @Expose()
+  @ApiProperty({
+    example: ' 일요일 휴관',
+    description: '전시 휴무일',
+  })
+  @Transform(({ value }) => value ?? undefined)
+  closed_days?: string;
+
+  @Expose()
+  @ApiProperty({
+    example: ' 무료',
+    description: '전시 입장료',
+  })
+  @Transform(({ value }) => value ?? undefined)
+  entrance_fee?: string;
+
+  @Expose()
+  @ApiProperty({
+    example: '서울특별시 성동구',
+    description: '전시/팝업 장소',
+  })
+  top_level_address: string;
 }
 
 export class PopupDto {
@@ -311,6 +356,13 @@ export class PopupDto {
 
   @Expose()
   @ApiProperty({
+    example: '팝업',
+    description: '장소 종류',
+  })
+  place_type: string;
+
+  @Expose()
+  @ApiProperty({
     example: '쫄깃즈 키링 팝업스토어',
     description: '장소 이름',
   })
@@ -326,6 +378,13 @@ export class PopupDto {
 
   @Expose()
   @ApiProperty({
+    example: '서울특별시 종로구 돈화문로11나길 28-1 1호 익선스페이스 A홀',
+    description: '주소',
+  })
+  address: string;
+
+  @Expose()
+  @ApiProperty({
     example: '2023-10-21 00:00:00',
     description: '시작일',
   })
@@ -337,4 +396,27 @@ export class PopupDto {
     description: '마감일',
   })
   end_date: Date;
+
+  @Expose()
+  @ApiProperty({
+    example: 'KANU(카누)',
+    description: '팝업 main 이름',
+  })
+  @Transform(({ value }) => value ?? undefined)
+  brandname?: string;
+
+  @Expose()
+  @ApiProperty({
+    example: '카누,커피,크리스마스,팝업스토어,미디어아트,시음,레이어57',
+    description: '팝업 해시태그',
+  })
+  @Transform(({ value }) => value ?? undefined)
+  hashtag?: string;
+
+  @Expose()
+  @ApiProperty({
+    example: '서울특별시 성동구',
+    description: '전시/팝업 장소',
+  })
+  top_level_address: string;
 }
