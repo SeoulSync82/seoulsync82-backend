fa840206e22eccea2579bd689c34f2fbfc581ac6
Blanc <topblade6@naver.com>
2023-12-12 03:54:02 +0900

    feature > 0.0.1 > Swagger - DTO res 데코레이터 설정 추가

---
+++ src/swagger.ts
@@ -0,0 +1,16 @@
+import { CoursePlaceDto, CourseRecommendResDto } from './course/dto/course.dto';
+import { CultureDto, CultureListDto, ExhibitionDto, PopupDto } from './place/dto/place.dto';
+import { SubwayCustomCheckResDto } from './place/dto/subway.dto';
+import { SearchDetailDto, SearchListDto } from './search/dto/search.dto';
+
+export const SwaggerModels = [
+  CultureListDto,
+  CultureDto,
+  PopupDto,
+  ExhibitionDto,
+  SearchDetailDto,
+  SearchListDto,
+  SubwayCustomCheckResDto,
+  CourseRecommendResDto,
+  CoursePlaceDto,
+];
