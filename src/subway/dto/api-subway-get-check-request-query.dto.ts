1d07074ccd2c019b0c68f7899d2881df3c24da8f
Blanc <topblade6@naver.com>
2025-02-04 02:08:59 +0900

    ⚡️ 지하철 역 커스텀 체크 API 개선
    ### API
    api/subway/:line_uuid/:station_uuid/customs-check
    
    req Applied `Transform` decorator to split `place_uuids` by commas

--- src/subway/dto/api-subway-get-check-request-query.dto.ts
+++ src/subway/dto/api-subway-get-check-request-query.dto.ts
@@ -1,16 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsArray, IsNotEmpty } from 'class-validator';
 
 export class ApiSubwayGetCheckRequestQueryDto {
   @IsNotEmpty()
   @IsArray()
+  @Transform(({ value }) => value.split(',')) // 콤마로 구분된 문자열을 배열로 변환
   @ApiProperty({
-    example: [
-      '00145054384a4b0d85b4198c6e54404f',
-      '00f239d44c7141e5942957c2219dd885',
-      '00f2fb983eec41a5a980747855752a6b',
-    ],
-    description: '장소 uuids',
+    example:
+      '00145054384a4b0d85b4198c6e54404f,00f239d44c7141e5942957c2219dd885,00f2fb983eec41a5a980747855752a6b',
+    description: '장소 uuids (콤마로 구분된 문자열)',
   })
   place_uuids: string[];
 }
